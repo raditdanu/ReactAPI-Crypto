5cdd5d823ef19c1c034b97a1cfa30590f5350706
raditdanu <radityadanue@gmail.com>
2022-04-11 21:58:11 +0700

    React API Crypto

---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Coin.css'
+
+const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) => {
+  return (
+    <div className='coin-container'>
+        <div className='coin-row'>
+            <div className='coin'>
+                <img src={image} alt="crypto" />
+                <h1>{name}</h1>
+                <p className='coin-symbol'>{symbol}</p>
+            </div>
+            <div className='coin-data'>
+                <p className='coin-price'>Rp {price.toLocaleString("id-ID")}</p>
+                <p className='coin-volume'>Rp {volume.toLocaleString("id-ID")}</p>
+                {priceChange < 0 ? (
+                    <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+                ) : (<p className='coin-percent green'>{priceChange.toFixed(2)}%</p>)
+            }
+            <p className='coin-marketcap'>
+                Mkt Cap: Rp {marketcap.toLocaleString("id-ID")}
+            </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Coin
